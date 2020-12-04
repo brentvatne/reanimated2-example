be4a8a25db0262d9a51dd77eabbeb0062c153cd2
Brent Vatne <brentvatne@gmail.com>
2020-12-03 19:41:41 -0800

    hello

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native";
+import Animated, {
+  useSharedValue,
+  withSpring,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  interpolate,
+} from "react-native-reanimated";
+import { PanGestureHandler } from "react-native-gesture-handler";
 
-export default function App() {
+function DragAndSnap() {
+  const translation = {
+    x: useSharedValue(0),
+    y: useSharedValue(0),
+  };
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.startX = translation.x.value;
+      ctx.startY = translation.y.value;
+    },
+    onActive: (event, ctx) => {
+      translation.x.value = ctx.startX + event.translationX;
+      translation.y.value = ctx.startY + event.translationY;
+    },
+    onEnd: (_) => {
+      translation.x.value = withSpring(0);
+      translation.y.value = withSpring(0);
+    },
+  });
+
+  const stylez = useAnimatedStyle(() => {
+    const H = Math.round(
+      interpolate(translation.x.value, [0, 300], [0, 360], "clamp")
+    );
+    const S = Math.round(
+      interpolate(translation.y.value, [0, 500], [100, 50], "clamp")
+    );
+    const backgroundColor = `hsl(${H},${S}%,50%)`;
+    return {
+      transform: [
+        {
+          translateX: translation.x.value,
+        },
+        {
+          translateY: translation.y.value,
+        },
+      ],
+      backgroundColor,
+    };
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, margin: 100 }}>
+      <PanGestureHandler onGestureEvent={gestureHandler}>
+        <Animated.View
+          style={[
+            {
+              width: 100,
+              height: 100,
+            },
+            stylez,
+          ]}
+        />
+      </PanGestureHandler>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default DragAndSnap;
